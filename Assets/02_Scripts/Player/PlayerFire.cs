b968f4b51d417137fded6182b2d997b30244b9e7
Oh-Nahyun <annieonh@gmail.com>
2026-09-02 15:02:57 +0900

    [Feat] 총알 쿨타임 추가

--- Assets/02_Scripts/Player/PlayerFire.cs
+++ Assets/02_Scripts/Player/PlayerFire.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class PlayerFire : MonoBehaviour
@@ -7,21 +8,46 @@ public class PlayerFire : MonoBehaviour
     // - 총알 프리팹
     public GameObject BulletPrefab;
     // - 생성 위치(총구)
-    public Transform FirePoint1;
-    public Transform FirePoint2;
-    
+    public Transform FirePointLeft;
+    public Transform FirePointRight;
+    // - 쿨타임
+    public float CoolTime;
+    private float lastTime;
+    private bool isFired = false;
+
+    private void Start()
+    {
+        lastTime = CoolTime;
+    }
+
     private void Update()
     {
+        if (isFired)
+        {
+            lastTime -= Time.deltaTime;
+            Debug.Log($"남은 쿨타임 시간 : {lastTime}");
+            
+            if (lastTime <= 0)
+            {
+                lastTime = CoolTime;
+                isFired = false;
+                Debug.Log("총알 발사 가능!");
+            }
+        }
+        
         // 1. 스페이스바를 누른다.
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (!isFired && Input.GetKeyDown(KeyCode.Space))
         {
             // 2. 총알 프리팹을 생성한다.
             // Instantiate는 프리팹을 복사해서 (MonoBehaviour를 상속받는) 게임 오브젝트를 생성하고 씬에 넣어주는 기능
-            GameObject bullet1 = Instantiate(BulletPrefab);
-            bullet1.transform.position = FirePoint1.position; // 생성한 총알의 위치를 총구의 위치로 이동
+            GameObject bulletLeft = Instantiate(BulletPrefab);
+            bulletLeft.transform.position = FirePointLeft.position; // 생성한 총알의 위치를 총구의 위치로 이동
             
-            GameObject bullet2 = Instantiate(BulletPrefab);
-            bullet2.transform.position = FirePoint2.position;
+            GameObject bulletRight = Instantiate(BulletPrefab);
+            bulletRight.transform.position = FirePointRight.position;
+
+            isFired = true;
+            Debug.Log("총알 발사 완료!");
         }
     }
 }
