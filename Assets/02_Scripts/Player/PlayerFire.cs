79e1ea9ca473d65ae33bb8c80e30cf25e7228a4c
Oh-Nahyun <annieonh@gmail.com>
2026-09-02 14:44:36 +0900

    [Feat] 총알 양쪽 발사 기능 추가

--- Assets/02_Scripts/Player/PlayerFire.cs
+++ Assets/02_Scripts/Player/PlayerFire.cs
@@ -7,7 +7,8 @@ public class PlayerFire : MonoBehaviour
     // - 총알 프리팹
     public GameObject BulletPrefab;
     // - 생성 위치(총구)
-    public Transform FirePoint;
+    public Transform FirePoint1;
+    public Transform FirePoint2;
     
     private void Update()
     {
@@ -16,8 +17,11 @@ public class PlayerFire : MonoBehaviour
         {
             // 2. 총알 프리팹을 생성한다.
             // Instantiate는 프리팹을 복사해서 (MonoBehaviour를 상속받는) 게임 오브젝트를 생성하고 씬에 넣어주는 기능
-            GameObject bullet = Instantiate(BulletPrefab);
-            bullet.transform.position = FirePoint.position; // 생성한 총알의 위치를 총구의 위치로 이동
+            GameObject bullet1 = Instantiate(BulletPrefab);
+            bullet1.transform.position = FirePoint1.position; // 생성한 총알의 위치를 총구의 위치로 이동
+            
+            GameObject bullet2 = Instantiate(BulletPrefab);
+            bullet2.transform.position = FirePoint2.position;
         }
     }
 }
