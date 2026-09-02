fc53952fc57135a38f97142beb32666cda185223
Oh-Nahyun <annieonh@gmail.com>
2026-09-02 15:32:22 +0900

    [Feat] 보조 총알 양쪽 2개 추가 (보조 총알은 속도와 모양이 다름)

--- Assets/02_Scripts/Player/PlayerFire.cs
+++ Assets/02_Scripts/Player/PlayerFire.cs
@@ -6,10 +6,13 @@ public class PlayerFire : MonoBehaviour
     // 목표 : 스페이스바를 누를 때마다 총알을 생성해서 발사하고 싶다.
     // 필요 속성
     // - 총알 프리팹
-    public GameObject BulletPrefab;
+    public GameObject BulletFrontPrefab;
+    public GameObject BulletBackPrefab;
     // - 생성 위치(총구)
-    public Transform FirePointLeft;
-    public Transform FirePointRight;
+    public Transform FirePointFrontLeft;
+    public Transform FirePointFrontRight;
+    public Transform FirePointBackLeft;
+    public Transform FirePointBackRight;
     // - 쿨타임
     public float CoolTime;
     private float lastTime;
@@ -37,11 +40,17 @@ public class PlayerFire : MonoBehaviour
         {
             // 2. 총알 프리팹을 생성한다.
             // Instantiate는 프리팹을 복사해서 (MonoBehaviour를 상속받는) 게임 오브젝트를 생성하고 씬에 넣어주는 기능
-            GameObject bulletLeft = Instantiate(BulletPrefab);
-            bulletLeft.transform.position = FirePointLeft.position; // 생성한 총알의 위치를 총구의 위치로 이동
+            GameObject bulletFrontLeft = Instantiate(BulletFrontPrefab);
+            bulletFrontLeft.transform.position = FirePointFrontLeft.position;
             
-            GameObject bulletRight = Instantiate(BulletPrefab);
-            bulletRight.transform.position = FirePointRight.position;
+            GameObject bulletFrontRight = Instantiate(BulletFrontPrefab);
+            bulletFrontRight.transform.position = FirePointFrontRight.position;
+            
+            GameObject bulletBackLeft = Instantiate(BulletBackPrefab);
+            bulletBackLeft.transform.position = FirePointBackLeft.position; // 생성한 총알의 위치를 총구의 위치로 이동
+            
+            GameObject bulletBackRight = Instantiate(BulletBackPrefab);
+            bulletBackRight.transform.position = FirePointBackRight.position;
 
             isFired = true;
             Debug.Log("총알 발사 완료!");
