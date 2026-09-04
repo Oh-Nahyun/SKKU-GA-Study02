eaf62103bcec9c7d6284d150a8a6ff18854f814e
Oh-Nahyun <annieonh@gmail.com>
2026-09-04 11:01:40 +0900

    refactor: 적 find 위치 수정 및 플레이어가 null인 경우에 대한 코드 추가

--- Assets/02_Scripts/Enemy/EnemyFollowPlayer.cs
+++ Assets/02_Scripts/Enemy/EnemyFollowPlayer.cs
@@ -6,9 +6,18 @@ public class EnemyFollowPlayer : Enemy
     // 캐싱 : 자주 쓸법한 데이터(객체)를 가까운 곳에 저장해두고 쓰는 것
     private GameObject _player;
 
-    protected override void Move()
+    private void Start()
     {
         _player = GameObject.FindWithTag("Player");
+    }
+
+    protected override void Move()
+    {
+        if (_player == null)
+        {
+            return;
+        }
+
         Vector2 direction = _player.transform.position - transform.position;
         direction.Normalize();
         transform.Translate(direction * _moveSpeed * Time.deltaTime);
--- Assets/02_Scripts/Enemy/EnemyGoToPlayer.cs
+++ Assets/02_Scripts/Enemy/EnemyGoToPlayer.cs
@@ -8,13 +8,21 @@ public class EnemyGoToPlayer : Enemy
 
     private void Start()
     {
-        _direction = _player.transform.position - transform.position;
-        _direction.Normalize();
+        _player = GameObject.FindWithTag("Player");
+
+        if (_player != null)
+        {
+            _direction = _player.transform.position - transform.position;
+            _direction.Normalize();
+        }
+        else
+        {
+            Debug.LogWarning("플레이어를 찾을 수 없어 방향을 설정하지 못했습니다.");
+        }
     }
 
     protected override void Move()
     {
-        _player = GameObject.FindWithTag("Player");
         transform.Translate(_direction * _moveSpeed * Time.deltaTime);
     }
 }
