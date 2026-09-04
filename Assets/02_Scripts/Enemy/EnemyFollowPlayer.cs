76df825982292c4e14c2823159921f6a057e38df
Oh-Nahyun <annieonh@gmail.com>
2026-09-04 10:33:59 +0900

    chore: 플레이어를 따라가는 적의 플레이어 찾는 위치 수정

--- Assets/02_Scripts/Enemy/EnemyFollowPlayer.cs
+++ Assets/02_Scripts/Enemy/EnemyFollowPlayer.cs
@@ -5,13 +5,9 @@ public class EnemyFollowPlayer : Enemy
 {
     private GameObject _player;
 
-    private void Start()
-    {
-        _player = GameObject.FindWithTag("Player");
-    }
-
     protected override void Move()
     {
+        _player = GameObject.FindWithTag("Player");
         Vector2 direction = _player.transform.position - transform.position;
         direction.Normalize();
         transform.Translate(direction * _moveSpeed * Time.deltaTime);
