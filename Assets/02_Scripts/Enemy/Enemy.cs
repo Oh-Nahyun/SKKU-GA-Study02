cb71bed35e26cbf861641cc8daf2df1b5f81fbe8
Oh-Nahyun <annieonh@gmail.com>
2026-09-04 14:16:04 +0900

    chore: 적의 데미지 접근 제한자 수정

--- Assets/02_Scripts/Enemy/Enemy.cs
+++ Assets/02_Scripts/Enemy/Enemy.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 public abstract class Enemy : MonoBehaviour
 {
     [SerializeField] private int _health = 100;
+    [SerializeField] private int _damage = 100;
     [SerializeField] protected float _moveSpeed = 1f;
-    [SerializeField] protected int _damage = 100;
 
     private void Update()
     {
