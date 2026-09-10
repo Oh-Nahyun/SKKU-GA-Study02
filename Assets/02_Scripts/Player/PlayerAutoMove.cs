64f064b9e1491e5ca603ebf2d6a08d30e2b10da7
Oh-Nahyun <annieonh@gmail.com>
2026-09-10 11:14:36 +0900

    feat: 플레이어와 적과의 y축 차이에 의한 자동 이동 모드 제한 추가

--- Assets/02_Scripts/Player/PlayerAutoMove.cs
+++ Assets/02_Scripts/Player/PlayerAutoMove.cs
@@ -3,27 +3,38 @@ using UnityEngine;
 
 public class PlayerAutoMove : MonoBehaviour
 {
+    private Animator _animator;
+
     [SerializeField] private float _speed = 7f;
+    [SerializeField] private float _differenceY = 3f;
+    [SerializeField] private float _stopTrackingY = -2f;
 
     private GameObject[] _enemies;
     private GameObject _closestEnemy;
 
     private void Update()
     {
-        FindClosestEnemy();
+        FindClosestEnemy(); //
         AutoMove();
     }
 
     private void AutoMove()
     {
-        if (_closestEnemy == null)
+        if (_closestEnemy == null) return;
+
+        Vector3 diff = _closestEnemy.transform.position - transform.position;
+        Vector3 direction = diff;
+
+        // 플레이어와 적과의 y축 차이가 3보다 크면 앞으로 이동하고 아니라면 뒤로 이동
+        if (diff.y >= _differenceY)
         {
-            //Debug.Log("[AutoMode] 가장 가까운 적을 찾지 못했습니다.");
-            return;
+            direction.y = 1;
+        }
+        else
+        {
+            direction.y = -1;
         }
 
-        Vector2 direction = _closestEnemy.transform.position - transform.position;
-        direction.y = 0f;
         direction.Normalize();
         transform.Translate(direction * _speed * Time.deltaTime);
     }
@@ -31,6 +42,7 @@ public class PlayerAutoMove : MonoBehaviour
     private void FindClosestEnemy()
     {
         if (_closestEnemy != null) return;
+        if (_closestEnemy.transform.position.y > _stopTrackingY) return; //
 
         _enemies = GameObject.FindGameObjectsWithTag("Enemy");
         if (_enemies == null || _enemies.Length == 0) return;
@@ -38,8 +50,10 @@ public class PlayerAutoMove : MonoBehaviour
         float minDistance = float.MaxValue;
         _closestEnemy = _enemies[0];
 
-        foreach (var enemy in _enemies)
+        foreach (GameObject enemy in _enemies)
         {
+            if (enemy.transform.position.y < _stopTrackingY) continue;
+
             float distance = Vector3.Distance(transform.position, enemy.transform.position);
             distance = Mathf.Abs(distance);
 
