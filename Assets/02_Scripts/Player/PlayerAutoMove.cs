b2586d5a9743cc07d2a6b5f6068baca60cc6c22d
Oh-Nahyun <annieonh@gmail.com>
2026-09-10 10:51:32 +0900

    refactor: 플레이어 자동 이동 모드 클래스 가독성 향상

--- Assets/02_Scripts/Player/PlayerAutoMove.cs
+++ Assets/02_Scripts/Player/PlayerAutoMove.cs
@@ -3,30 +3,26 @@ using UnityEngine;
 
 public class PlayerAutoMove : MonoBehaviour
 {
-    private float _speed = 7f;
+    [SerializeField] private float _speed = 7f;
 
     private GameObject[] _enemies;
     private GameObject _closestEnemy;
 
     private void Update()
     {
-        _enemies = GameObject.FindGameObjectsWithTag("Enemy");
         FindClosestEnemy();
+        AutoMove();
+    }
 
+    private void AutoMove()
+    {
         if (_closestEnemy == null)
         {
             //Debug.Log("[AutoMode] 가장 가까운 적을 찾지 못했습니다.");
             return;
         }
 
-        AutoMove(_closestEnemy);
-    }
-
-    private void AutoMove(GameObject enemy)
-    {
-        if (enemy == null) return;
-
-        Vector2 direction = enemy.transform.position - transform.position;
+        Vector2 direction = _closestEnemy.transform.position - transform.position;
         direction.Normalize();
         direction.y = 0f;
         transform.Translate(direction * _speed * Time.deltaTime);
@@ -34,21 +30,23 @@ public class PlayerAutoMove : MonoBehaviour
 
     private void FindClosestEnemy()
     {
-        if (_enemies == null || _enemies.Length == 0) return;
-        _closestEnemy = _enemies[0];
+        if (_closestEnemy != null) return;
 
-        float minDistance = Vector3.Distance(transform.position, _closestEnemy.transform.position);
-        minDistance = Mathf.Abs(minDistance);
+        _enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        if (_enemies == null || _enemies.Length == 0) return;
+
+        float minDistance = float.MaxValue;
+        _closestEnemy = _enemies[0];
 
         foreach (var enemy in _enemies)
         {
             float distance = Vector3.Distance(transform.position, enemy.transform.position);
             distance = Mathf.Abs(distance);
 
-            if (minDistance >= distance)
+            if (minDistance > distance)
             {
-                _closestEnemy = enemy;
                 minDistance = distance;
+                _closestEnemy = enemy;
             }
         }
     }
