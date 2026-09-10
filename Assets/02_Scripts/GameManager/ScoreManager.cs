987479a9f6116f9487f0a87f89e04533c3e8eb80
Oh-Nahyun <annieonh@gmail.com>
2026-09-10 14:15:31 +0900

    feat: 플레이어 점수 갱신 로직 수정
    - 플레이어 점수는 처음과 점수가 변경되었을 때만 갱신되도록 수정

--- Assets/02_Scripts/GameManager/ScoreManager.cs
+++ Assets/02_Scripts/GameManager/ScoreManager.cs
@@ -10,7 +10,8 @@ public class ScoreManager : MonoBehaviour
 
     // 관리 : 특정 데이터에 대한 무결성과 생성, 읽기, 수정, 삭제 등과 관련된 로직
     private int _bestScore;
-    private int _currentScore;
+    private int _currentScore = 0;
+    private int _lastRefreshScore = -1;
 
     // UI 책임 추가
     [SerializeField] private TextMeshProUGUI _bestScoreTextUI;
@@ -35,8 +36,12 @@ public class ScoreManager : MonoBehaviour
 
     private void Refresh()
     {
+        if (_lastRefreshScore == _currentScore) return;
+
         _bestScoreTextUI.text = $"BestScore: {_bestScore}";
         _currentScoreTextUI.text = $"BestScore: {_currentScore}";
+
+        _lastRefreshScore = _currentScore;
     }
 
     public int GetScore()
